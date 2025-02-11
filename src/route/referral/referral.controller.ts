5a7ae0e1874b26f5096af6677c0655f0088cd2c4
cloudred <cloudred442@gmail.com>
2025-02-11 10:11:46 +0800

    enwest commit

--- src/route/referral/referral.controller.ts
+++ src/route/referral/referral.controller.ts
@@ -68,6 +68,7 @@ export const referralTotalGetController = async (c: Context) => {
 
     return c.json({ message: "Data fetched successfully", data });
   } catch (error) {
+    console.log(error);
     return sendErrorResponse("Internal Server Error", 500);
   }
 };
