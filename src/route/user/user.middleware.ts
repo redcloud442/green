e7a329de8010cf6bd3927145cfb3b38c86dbdd76
redcloud442 <cloudred442@gmail.com>
2025-04-02 14:32:55 +0800

    newest commit

--- src/route/user/user.middleware.ts
+++ src/route/user/user.middleware.ts
@@ -16,6 +16,7 @@ import {
 import { sendErrorResponse } from "../../utils/function.js";
 import prisma from "../../utils/prisma.js";
 import {
+    protectionAccountingMerchantAdmin,
   protectionAdmin,
   protectionMemberUser,
 } from "../../utils/protection.js";
@@ -594,7 +595,7 @@ export const userListReinvestedMiddleware = async (c: Context, next: Next) => {
 export const userTreeMiddleware = async (c: Context, next: Next) => {
   const user = c.get("user");
 
-  const response = await protectionAdmin(user.id, prisma);
+  const response = await protectionAccountingMerchantAdmin(user.id, prisma);
 
   if (response instanceof Response) {
     return response;
--- src/utils/protection.ts
+++ src/utils/protection.ts
@@ -185,6 +185,49 @@ export const protectionAdmin = async (userId: string, prisma: PrismaClient) => {
   }
 };
 
+export const protectionAccountingMerchantAdmin = async (userId: string, prisma: PrismaClient) => {
+    try {
+      const user = await prisma.user_table.findUnique({
+        where: { user_id: userId },
+        select: {
+          user_id: true,
+
+          alliance_member_table: {
+            select: {
+              alliance_member_id: true,
+              alliance_member_role: true,
+              alliance_member_restricted: true,
+              alliance_member_is_active: true,
+              alliance_member_alliance_id: true,
+            },
+          },
+        },
+      });
+
+      if (!user) {
+        return sendErrorResponse("Internal Server Error", 500);
+      }
+
+    if (
+      !["ADMIN", "ACCOUNTING", "ACCOUNTING_HEAD","MERCHANT"].includes(
+        user.alliance_member_table[0].alliance_member_role
+      )
+    ) {
+      return sendErrorResponse("Invalid Referral Link", 400);
+    }
+
+      if (user.alliance_member_table[0].alliance_member_restricted) {
+        return sendErrorResponse("Internal Server Error", 500);
+      }
+
+      return {
+        teamMemberProfile: user.alliance_member_table[0] as alliance_member_table,
+      };
+    } catch (e) {
+      return sendErrorResponse("Internal Server Error", 500);
+    }
+  };
+
 export const protectionClient = async (
   userId: string,
   prisma: PrismaClient
