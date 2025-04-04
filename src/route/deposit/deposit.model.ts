1b602976a7821d22bd559edf8c591dd338fd33bb
cloudred <cloudred442@gmail.com>
2025-04-04 20:25:27 +0800

    newest commit

--- src/route/deposit/deposit.model.ts
+++ src/route/deposit/deposit.model.ts
@@ -485,45 +485,47 @@ OFFSET ${Prisma.raw(offset.toString())}
     returnData.merchantBalance = merchant?.merchant_member_balance;
   }
 
-  const totalPendingDeposit =
-    await prisma.alliance_top_up_request_table.aggregate({
-      _sum: {
-        alliance_top_up_request_amount: true,
-      },
-      where: {
-        alliance_top_up_request_status: "PENDING",
-        alliance_top_up_request_date:
-          dateFilter.start && dateFilter.end
-            ? {
-                gte: getPhilippinesTime(new Date(dateFilter.start), "start"),
-                lte: getPhilippinesTime(new Date(dateFilter.end), "end"),
-              }
-            : undefined,
-      },
-    });
+  if (teamMemberProfile.alliance_member_role === "MERCHANT") {
+    const totalPendingDeposit =
+      await prisma.alliance_top_up_request_table.aggregate({
+        _sum: {
+          alliance_top_up_request_amount: true,
+        },
+        where: {
+          alliance_top_up_request_status: "PENDING",
+          alliance_top_up_request_date:
+            dateFilter.start && dateFilter.end
+              ? {
+                  gte: getPhilippinesTime(new Date(dateFilter.start), "start"),
+                  lte: getPhilippinesTime(new Date(dateFilter.end), "end"),
+                }
+              : undefined,
+        },
+      });
 
-  const totalApprovedDeposit =
-    await prisma.alliance_top_up_request_table.aggregate({
-      _sum: {
-        alliance_top_up_request_amount: true,
-      },
-      where: {
-        alliance_top_up_request_status: "APPROVED",
-        alliance_top_up_request_date:
-          dateFilter.start && dateFilter.end
-            ? {
-                gte: getPhilippinesTime(new Date(dateFilter.start), "start"),
-                lte: getPhilippinesTime(new Date(dateFilter.end), "end"),
-              }
-            : undefined,
-      },
-    });
+    const totalApprovedDeposit =
+      await prisma.alliance_top_up_request_table.aggregate({
+        _sum: {
+          alliance_top_up_request_amount: true,
+        },
+        where: {
+          alliance_top_up_request_status: "APPROVED",
+          alliance_top_up_request_date:
+            dateFilter.start && dateFilter.end
+              ? {
+                  gte: getPhilippinesTime(new Date(dateFilter.start), "start"),
+                  lte: getPhilippinesTime(new Date(dateFilter.end), "end"),
+                }
+              : undefined,
+        },
+      });
 
-  returnData.totalPendingDeposit =
-    totalPendingDeposit._sum.alliance_top_up_request_amount || 0;
+    returnData.totalPendingDeposit =
+      totalPendingDeposit._sum.alliance_top_up_request_amount || 0;
 
-  returnData.totalApprovedDeposit =
-    totalApprovedDeposit._sum.alliance_top_up_request_amount || 0;
+    returnData.totalApprovedDeposit =
+      totalApprovedDeposit._sum.alliance_top_up_request_amount || 0;
+  }
 
   return JSON.parse(
     JSON.stringify(returnData, (key, value) =>
