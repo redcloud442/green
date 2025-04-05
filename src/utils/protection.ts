815eef583ec4a627161f316fbc95774bcca013b0
cloudred <cloudred442@gmail.com>
2025-04-05 11:51:00 +0800

    newest commit

--- src/utils/protection.ts
+++ src/utils/protection.ts
@@ -10,7 +10,7 @@ export const protectionMemberUser = async (
       where: { user_id: userId },
       select: {
         user_id: true,
-
+        user_username: true,
         alliance_member_table: {
           select: {
             alliance_member_id: true,
@@ -185,48 +185,51 @@ export const protectionAdmin = async (userId: string, prisma: PrismaClient) => {
   }
 };
 
-export const protectionAccountingMerchantAdmin = async (userId: string, prisma: PrismaClient) => {
-    try {
-      const user = await prisma.user_table.findUnique({
-        where: { user_id: userId },
-        select: {
-          user_id: true,
+export const protectionAccountingMerchantAdmin = async (
+  userId: string,
+  prisma: PrismaClient
+) => {
+  try {
+    const user = await prisma.user_table.findUnique({
+      where: { user_id: userId },
+      select: {
+        user_id: true,
 
-          alliance_member_table: {
-            select: {
-              alliance_member_id: true,
-              alliance_member_role: true,
-              alliance_member_restricted: true,
-              alliance_member_is_active: true,
-              alliance_member_alliance_id: true,
-            },
+        alliance_member_table: {
+          select: {
+            alliance_member_id: true,
+            alliance_member_role: true,
+            alliance_member_restricted: true,
+            alliance_member_is_active: true,
+            alliance_member_alliance_id: true,
           },
         },
-      });
+      },
+    });
 
-      if (!user) {
-        return sendErrorResponse("Internal Server Error", 500);
-      }
+    if (!user) {
+      return sendErrorResponse("Internal Server Error", 500);
+    }
 
     if (
-      !["ADMIN", "ACCOUNTING", "ACCOUNTING_HEAD","MERCHANT"].includes(
+      !["ADMIN", "ACCOUNTING", "ACCOUNTING_HEAD", "MERCHANT"].includes(
         user.alliance_member_table[0].alliance_member_role
       )
     ) {
       return sendErrorResponse("Invalid Referral Link", 400);
     }
 
-      if (user.alliance_member_table[0].alliance_member_restricted) {
-        return sendErrorResponse("Internal Server Error", 500);
-      }
-
-      return {
-        teamMemberProfile: user.alliance_member_table[0] as alliance_member_table,
-      };
-    } catch (e) {
+    if (user.alliance_member_table[0].alliance_member_restricted) {
       return sendErrorResponse("Internal Server Error", 500);
     }
-  };
+
+    return {
+      teamMemberProfile: user.alliance_member_table[0] as alliance_member_table,
+    };
+  } catch (e) {
+    return sendErrorResponse("Internal Server Error", 500);
+  }
+};
 
 export const protectionClient = async (
   userId: string,
